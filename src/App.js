5aee5a0c793f2d1fada65614561e1bdd1c32cce4
yuki hara <hara.0509@icloud.com>
2021-02-08 15:47:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchForm from './components/SearchForm';
+import Weather from './Api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/forecast'
+
+const App = ()=> {
+    return(
+        <div className='container'>
+            <h1>お天気検索</h1>
+            {/* <SearchForm/> */}
+            <Weather/>
+        </div>
+    )
 }
 
 export default App;
